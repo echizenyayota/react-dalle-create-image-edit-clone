81522048bae2073ca4521922a7b8ed281114c7d5
echizenyayota <virtualmisso30@gmail.com>
2023-12-20 17:54:06 +0900

    4:11 Place the necessary codes in the search-section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 const App = () => {
+
   return (
     <div className="app">
-
+      <section className="prompt-section">
+        <p>Give a detailed description</p>
+        <div className="input-container">
+          <input 
+            placeholder="An impression oil painting of a sunflower in a purple vase..."
+          />
+          <button>Edit</button>
+        </div>
+      </section>
+      <section className="prompt-section"></section>
     </div>
   );
 }
