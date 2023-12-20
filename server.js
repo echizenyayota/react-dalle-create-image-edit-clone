ce8ad0e9dbe2cc08990d1842a886e7d6dbf9162b
echizenyayota <virtualmisso30@gmail.com>
2023-12-20 20:17:07 +0900

    22:32 Launch a web server on PORT 8000

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 const PORT = 8000;
 const express = require("express");
+
+require("dotenv").config();
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => console.log("Your server is running on PORT " + PORT))
