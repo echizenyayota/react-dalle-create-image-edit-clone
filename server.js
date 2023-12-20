aaf958af24fab3393e5ba15dfabaeb2a70fd08c7
echizenyayota <virtualmisso30@gmail.com>
2023-12-20 21:55:03 +0900

    24:44 Set the route to post an image and edit an prompt

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const PORT = 8000;
+const OpenAI  = require("openai");
 const express = require("express");
 
 require("dotenv").config();
+const openai = new OpenAI();
 const app = express();
 const cors = require("cors");
 app.use(cors());
@@ -11,6 +13,19 @@ app.get("/", (req, res) => {
   res.status(200).json({
     message: "Testing whether the API works",
   }); 
+});
+
+app.post("/image_edit", async (req, res) => {
+  try {
+    const response = await openai.images.edit({
+      image: fs.createReadStream("otter.png"),
+      prompt: "A cute baby sea otter wearing a beret",
+    });
+    console.log(response.data);
+    res.send(response.data);
+  } catch(error) {
+    console.error(error);
+  }
 });
 
 app.listen(PORT, () => console.log("Your server is running on PORT " + PORT));
