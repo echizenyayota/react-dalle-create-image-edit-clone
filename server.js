c4e9fc24d5d4adcfb964b48e711f120362f4cebd
echizenyayota <virtualmisso30@gmail.com>
2023-12-20 20:27:19 +0900

    22:32 Set the route

--- server.js
+++ server.js
@@ -7,4 +7,10 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT, () => console.log("Your server is running on PORT " + PORT))
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Testing whether the API works",
+  }); 
+});
+
+app.listen(PORT, () => console.log("Your server is running on PORT " + PORT));
