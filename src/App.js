0e993cd7b4b13db98131c1ce50417383e96c6043
echizenyayota <virtualmisso30@gmail.com>
2024-01-04 16:54:58 +0900

    Place the elements to upload the image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 const App = () => {
+
+  const uploadImage = () => {
+    
+  }
+
   return (
     <div className="app">
-
+      <div className="upload-image">
+        <span>
+          <label htmlFor="files">Upload an image </label>
+          <input onChange={uploadImage} id="files" accept="imgae/*" type="file" hidden />
+          to edit.
+        </span>
+      </div>
     </div>
   );
 }
