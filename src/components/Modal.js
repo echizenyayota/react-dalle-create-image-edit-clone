ef6a55a93e66b6e5dc9e7cc8ad3cb5aee78399e2
echizenyayota <virtualmisso30@gmail.com>
2024-01-07 19:34:52 +0900

    Set the image size as 256 * 256 to getEditImage

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 
 const Modal = ({ setModalOpen, setSelectedImage, selectedImage}) => {
 
   const [error, setError] = useState(null);
+  const ref = useRef(null);
   
   console.log('selectedImage', selectedImage);
   
@@ -12,14 +13,24 @@ const Modal = ({ setModalOpen, setSelectedImage, selectedImage}) => {
     setSelectedImage(null);
   }
 
+  const checkSize = () => {
+    if (ref.current.width === 256 && ref.current.height === 256) {
+      // getEditImage();
+    } else {
+      setError('Error: Choose 256 * 256 image');
+    }
+  }
+
   return(
     <div className="modal">
       <div onClick={closeModal}>X</div>
-      <div className="img-container">
-        {selectedImage && <img src={URL.createObjectURL(selectedImage)} alt="uploaded image" />}
+      <div className="image-container">
+        {selectedImage && <img ref={ref} src={URL.createObjectURL(selectedImage)} alt="uploaded image" />}
       </div>
-      <textarea />
-      <button>Send</button>
+      <p>{error || "* Image must be 256 * 256!"}</p>
+      {! error && <textarea />}
+      {! error && <button onClick={checkSize}>Send</button>}
+      {error && <button onClick={closeModal}>Close this and try again</button>}
     </div>
   );
 }
